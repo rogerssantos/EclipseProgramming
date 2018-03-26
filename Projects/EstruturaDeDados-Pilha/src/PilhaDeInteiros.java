261f4833f3311df491ed38fe0189a936dddabcb2
Roger Santos <roger_santos@outlook.com>
2018-03-25 23:44:18 -0300

    .

---
+++ Projects/EstruturaDeDados-Pilha/src/PilhaDeInteiros.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class PilhaDeInteiros {
+	public int[] pilha;
+	public int indicePilha;
+	
+	public PilhaDeInteiros() {
+		this.indicePilha = -1;
+		this.pilha = new int [100];
+
+	}
+	
+	public boolean estaVazia() {
+		if (this.indicePilha > -1);
+			return false;
+	}
+	
+	public int tamanho() {
+		return this.indicePilha+1;
+	}
+	
+	
+	public int mostraTopo() {
+		return pilha[this.indicePilha];
+	}
+	
+	public int desempilhar() {
+		Arrays.copyOf(this.pilha, this.pilha.length-1);
+		--this.indicePilha;
+		return tamanho();
+	}
+	
+	public void empilhar(int valor) {
+		this.pilha[this.indicePilha+1] = valor;
+		++this.indicePilha;
+	}
+	
+}
---
+++ Projects/EstruturaDeDados-Pilha/src/UsaPilhaDeInteiros.java
@@ -0,0 +1,24 @@
+
+public class UsaPilhaDeInteiros{
+	public static void main(String args[]) {
+		PilhaDeInteiros p = new PilhaDeInteiros();
+		System.out.println(p.estaVazia());
+		p.empilhar(10);
+		System.out.println(p.tamanho());
+		System.out.println(p.mostraTopo());
+		System.out.println(p.estaVazia());
+		p.empilhar(15);
+		p.empilhar(9);
+		p.empilhar(46);
+		p.empilhar(7);
+		System.out.println(p.tamanho());
+		System.out.println(p.mostraTopo());
+		System.out.println(p.estaVazia());
+		System.out.println(p.tamanho());
+		System.out.println(p.desempilhar());
+		System.out.println(p.tamanho());
+			while (p.estaVazia() == false) {
+				System.out.println(p.desempilhar());
+			}
+	}
+}
