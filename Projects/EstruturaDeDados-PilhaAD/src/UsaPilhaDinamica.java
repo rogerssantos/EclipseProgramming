01d5989cf8e39e78070f4527a885dc4c917aa06f
Roger Santos <roger_santos@outlook.com>
2018-04-02 18:43:45 -0300

    .

--- Projects/EstruturaDeDados-PilhaAD/src/UsaPilhaDinamica.java
+++ Projects/EstruturaDeDados-PilhaAD/src/UsaPilhaDinamica.java
@@ -4,13 +4,14 @@ public class UsaPilhaDinamica {
 	public static void main(String[] args) {
 
 		PilhaDinamica p = new PilhaDinamica();
+		System.out.println("Empilhando...");
 		p.empilha(1);
 		p.empilha(2);
 		p.empilha(3);
 		p.empilha(4);
-		p.empilha(5);
 		while (p.estaVazia() == false) {
 			System.out.println(p.desempilhar());
 		}
+		
 	}
 }
