89b762837a04e6611a3bd58a9835665ba8a48d57
Roger Santos <roger_santos@outlook.com>
2018-04-03 21:24:08 -0300

    .

--- Projects/EstruturaDeDados-PilhaAD-Calculadora/src/UsaPilhaADCalculadora.java
+++ Projects/EstruturaDeDados-PilhaAD-Calculadora/src/UsaPilhaADCalculadora.java
@@ -3,18 +3,18 @@ public class UsaPilhaADCalculadora {
 
 	public static void main(String[] args) {
 
-		//Implemente o funcionamento de calculadoras capazes de resolver expressıes.
-		//Elas trabalham com expressıes pÛs-fixadas, ent„o para avaliarmos uma express„o
-		//como (1-2)*(4+5) podemos digitar 1 2 ñ 4 5 + *. O funcionamento dessas
-		//calculadoras È muito simples. Cada operando È empilhado numa pilha de valores.
-		//Quando se encontra um operador, desempilha-se o n˙mero apropriado de operandos
-		//(dois para operadores bin·rios e um para operadores un·rios), realiza-se a operaÁ„o
-		//devida e empilha-se o resultado. Deste modo, na express„o acima, s„o empilhados
-		//os valores 1 e 2. Quando aparece o operador -, 1 e 2 s„o desempilhados e o
-		//resultado da operaÁ„o, no caso -1 (= 1 - 2), È colocado no topo da pilha.
-		//A seguir, 4 e 5 s„o empilhados. O operador seguinte, +, desempilha o 4 e o 5
-		//e empilha o resultado da soma, 9. Nesta hora, est„o na pilha os dois resultados
-		//parciais, -1 na base e 9 no topo. O operador *, ent„o, desempilha os dois e
+		//Implemente o funcionamento de calculadoras capazes de resolver express√µes.
+		//Elas trabalham com express√µes p√≥s-fixadas, ent√£o para avaliarmos uma express√£o
+		//como (1-2)*(4+5) podemos digitar 1 2 ‚Äì 4 5 + *. O funcionamento dessas
+		//calculadoras √© muito simples. Cada operando √© empilhado numa pilha de valores.
+		//Quando se encontra um operador, desempilha-se o n√∫mero apropriado de operandos
+		//(dois para operadores bin√°rios e um para operadores un√°rios), realiza-se a opera√ß√£o
+		//devida e empilha-se o resultado. Deste modo, na express√£o acima, s√£o empilhados
+		//os valores 1 e 2. Quando aparece o operador -, 1 e 2 s√£o desempilhados e o
+		//resultado da opera√ß√£o, no caso -1 (= 1 - 2), √© colocado no topo da pilha.
+		//A seguir, 4 e 5 s√£o empilhados. O operador seguinte, +, desempilha o 4 e o 5
+		//e empilha o resultado da soma, 9. Nesta hora, est√£o na pilha os dois resultados
+		//parciais, -1 na base e 9 no topo. O operador *, ent√£o, desempilha os dois e
 		//coloca -9 (= -1 * 9) no topo da pilha. Implemente para os operadores + - * / .
 		
 		PilhaADCalculadora p = new PilhaADCalculadora();
@@ -22,12 +22,9 @@ public class UsaPilhaADCalculadora {
 		p.empilha("1");
 		p.empilha("2");
 		p.empilha("+");
-		p.empilha("15");
-		p.empilha("-");
 		p.empilha("10");
 		p.empilha("/");
 		p.empilha("5");
-		p.empilha("+");
 		
 	}
 }
